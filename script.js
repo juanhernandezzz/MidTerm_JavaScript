3ef8971c56f111d00d59fec70cf5639f7f89a8bd
juanhernandezzz <157745812+juanhernandezzz@users.noreply.github.com>
2024-03-05 12:49:40 -0600

    add a button demonstration

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.createElement("button");
+
+// Create button text
+const buttonText = new Text("Click Here!!!");
+
+//Put button text in button
+button.appendChild(buttonText);
+
+// Put button in document's body
+document.body.appendChild(button);
